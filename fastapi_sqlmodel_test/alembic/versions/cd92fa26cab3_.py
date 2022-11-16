7e06f7b4b67553da9e45fd3a6e00b3f0b14bc1a7
Demian Oyarzún <demianoyarzunmancilla@gmail.com>
2022-11-15 19:34:14 -0500

    Generación de alembic

---
+++ fastapi_sqlmodel_test/alembic/versions/cd92fa26cab3_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: cd92fa26cab3
+Revises: 
+Create Date: 2022-11-15 19:30:58.873718
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cd92fa26cab3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
